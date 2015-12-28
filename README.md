1da6348deb46c5915c913cab34b055adfeb8066f
astriker <astriker@gmail.com>
2015-12-29 01:13:29 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # googleapp
 google app engine
+
+
+At window cmd:
+run
+python "C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" C:\SVN\aligthome\PyCharm\googleapp\
+
+deploy
+python appcfg.py -A test-cloud-1172 update C:\SVN\aligthome\PyCharm\googleapp\
---
+++ helloworld.py
@@ -0,0 +1,20 @@
+from google.appengine.api import users
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+# python dev_appserver.py C:\SVN\aligthome\PyCharm\hellowapp\
+# python appcfg.py -A test-cloud-1172 update C:\SVN\aligthome\PyCharm\hellowapp\
